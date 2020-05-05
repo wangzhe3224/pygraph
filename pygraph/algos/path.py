622564afaba6681435acc021b305dd8fa7e4a8a0
Zhe Wang <wangzhetju@gmail.com>
2020-05-05 23:50:34 +0100

    add dfs path search

--- pygraph/algos/path.py
+++ pygraph/algos/path.py
@@ -1,6 +1,12 @@
 from pygraph.entities.graph import UndirectedGraph
 
 
+class DfsPath:
+
+    def __call__(self, graph, source):
+        """"""
+
+
 def dfs_path(graph, source):
     """ get paths from source to other nodes.
 
@@ -11,3 +17,33 @@ def dfs_path(graph, source):
     :return:
     {target: [source, x, x, node2]}
     """
+    visited = set()
+    edge_to = {}  # magic path..
+
+    def _dfs_path(graph, start):
+        for _node in graph[start]:  # all its neighbour
+            if _node in visited:
+                continue
+            visited.add(_node)
+            edge_to[_node] = start
+            _dfs_path(graph, _node)
+
+    # DFS
+    _dfs_path(graph, source)
+
+    paths = {}
+    # find path
+    for node in graph.nodes:
+        if node in edge_to:  # has a path
+            path = []
+            _next = node
+            while _next != source:
+                path.append(_next)
+                _next = edge_to[_next]
+            path.append(source)
+            paths[node] = path
+
+        else:
+            paths[node] = None   # no path
+
+    return paths
---
+++ tests/test_path.py
@@ -0,0 +1,13 @@
+import unittest
+
+from pygraph.algos.path import dfs_path
+from pygraph.entities.utils import *
+
+
+class TestPath(unittest.TestCase):
+
+    def test_dfs_path(self):
+        gp = get_sample_graph()
+        paths = dfs_path(gp, 'a')
+        tar = {'a': ['a'], 'b': ['b', 'a'], 'c': ['c', 'a'], 'd': ['d', 'c', 'a'], 'e': ['e', 'b', 'a']}
+        self.assertDictEqual(paths, tar)
