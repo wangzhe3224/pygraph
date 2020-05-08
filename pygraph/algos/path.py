bc6b617f2728f92e473ce19cb49f839f5839f1b0
Zhe Wang <wangzhetju@gmail.com>
2020-05-08 16:57:51 +0100

    add bfs path

--- pygraph/algos/path.py
+++ pygraph/algos/path.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 from pygraph.entities.graph import UndirectedGraph
 
 
@@ -11,6 +13,7 @@ class DfsPath:
 def dfs_path(graph, source):
     """ get paths from source to other nodes.
 
+    edge_to is a parent-link representation of the tree which has source as root.
     Note: not all the path, but one of the path if exist
 
     :param graph: Graph
@@ -32,9 +35,42 @@ def dfs_path(graph, source):
     # DFS
     _dfs_path(graph, source)
 
-    paths = {}
-    # find path
-    for node in graph.nodes:
+    return path_view(graph.nodes, edge_to, source)
+
+
+def bfs_path(graph, source):
+    """ a breadth first search for paths. These suppose to be the shortest paths.
+
+    edge_to is a parent-link representation of the tree which has source as root.
+
+    Reference
+    ---------
+    <Algorithms 4th edition> by Robert Sedgewick. P540
+
+    :param source: a source node
+    """
+    _queue = deque([])
+    _visited = set()
+    _queue.append(source)
+    _edge_to = {source: source}
+    while _queue:
+        cur_node = _queue.popleft()
+        for child in graph[cur_node]:
+            if child in _visited:
+                continue
+
+            _visited.add(cur_node)
+            _edge_to[child] = cur_node
+            _queue.append(child)
+
+    return path_view(graph.nodes, _edge_to, source)
+
+
+def path_view(nodes, edge_to: dict, source):
+    """ convert edge_to to path view """
+    _paths = {}
+
+    for node in nodes:
         if node in edge_to:  # has a path
             path = []
             _next = node
@@ -42,12 +78,9 @@ def dfs_path(graph, source):
                 path.append(_next)
                 _next = edge_to[_next]
             path.append(source)
-            paths[node] = path
+            _paths[node] = path
 
         else:
-            paths[node] = None   # no path
+            _paths[node] = None   # no path
 
-    return paths
-
-def bfs_path(graph, source):
-    """"""
+    return _paths
--- tests/test_path.py
+++ tests/test_path.py
@@ -1,6 +1,6 @@
 import unittest
 
-from pygraph.algos.path import dfs_path
+from pygraph.algos.path import dfs_path, bfs_path
 from pygraph.entities.utils import *
 
 
@@ -11,3 +11,9 @@ class TestPath(unittest.TestCase):
         paths = dfs_path(gp, 'a')
         tar = {'a': ['a'], 'b': ['b', 'a'], 'c': ['c', 'a'], 'd': ['d', 'c', 'a'], 'e': ['e', 'b', 'a']}
         self.assertDictEqual(paths, tar)
+
+    def test_bfs_path(self):
+        gp = get_sample_graph()
+        paths = bfs_path(gp, 'a')
+        tar = {'a': ['a'], 'b': ['b', 'a'], 'c': ['c', 'a'], 'd': ['d', 'c', 'a'], 'e': ['e', 'b', 'a']}
+        self.assertDictEqual(tar, paths)
