297b582e1219a825a1c307710126a2bb3235a0cb
Zhe Wang <wangzhetju@gmail.com>
2020-05-08 21:28:59 +0100

    add doc

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,8 @@ author = 'Zhe'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   'sphinx.ext.githubpages'
+   'sphinx.ext.githubpages',
+   'sphinx.ext.intersphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
